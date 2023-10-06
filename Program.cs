9d1f5e3aaf0eab9faaca1a5340641ecbfff907b5
Paweł Kurek <pawel.kurek975@gmail.com>
2023-10-06 12:26:25 +0200

    init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"First argument is {args[0]}, and second one is {args[1]}");
+        }
+    }
+}
