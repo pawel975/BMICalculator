910e345bddca10bce57bacd0174c54dd88037cb9
Paweł Kurek <pawel.kurek975@gmail.com>
2023-10-07 13:58:45 +0200

    Created BMI calculator

--- Program.cs
+++ Program.cs
@@ -2,11 +2,58 @@
 
 namespace ConsoleApp
 {
+    public class BMICalulator
+    {
+        public static double CalculateBMI(double weightInKG, double heightInMeters)
+        {
+            return weightInKG / Math.Pow(heightInMeters, 2);
+        }
+
+        public static string GetBMIWeightStatus(double BMIIndex)
+        {
+            string BMIWeightStatus;
+
+            switch (BMIIndex) 
+            {
+                case < 18.5:
+                    BMIWeightStatus = "Underweight";
+                    break;
+                case < 25:
+                    BMIWeightStatus = "Normal range";
+                    break;
+                case < 30:
+                    BMIWeightStatus = "Overwieght";
+                    break;
+                case < 35:
+                    BMIWeightStatus = "Obese";
+                    break;
+                default:
+                    BMIWeightStatus = "Obese II";
+                    break;
+            }
+
+            return BMIWeightStatus;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"First argument is {args[0]}, and second one is {args[1]}");
+            Console.WriteLine("BMI Calculator");
+
+            Console.Write("Your mass in [kg]: ");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.Write("Your height in [m]: ");
+            double height = double.Parse(Console.ReadLine());
+
+            double BMIIndex = BMICalulator.CalculateBMI(weight, height);
+
+
+            Console.WriteLine($"Your BMI mass index is: {BMIIndex}");
+            Console.WriteLine($"BMI status is: {BMICalulator.GetBMIWeightStatus(BMIIndex)}");
+
+
         }
     }
 }
